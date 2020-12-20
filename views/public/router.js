10f9d92cbfdf82afb3083fcf06aa1d5c0b1f71c3
jeferson <jefersongomes333@hotmail.com>
2020-12-20 10:24:13 -0300

    tratamento de rotas do perfil

---
+++ views/public/router.js
@@ -0,0 +1,12 @@
+const vueRoutes = [
+  { path: '/', name: 'home', component: HomeComponent },
+  { path: '/services', name: 'services', component: JobComponent, props: route => ({ servicos: route.query.servicos }) },
+  { path: '/services/show/:id', name: 'services_show', component: servicoShow, props: route => ({ servicos: route.params.id }) },
+  { path: '/messages', name: 'messages', component: MessagesComponent, props: route => ({ mensagens: route.query.mensagens }) },
+  { path: '/configuration', component: ConfigComponent },
+];
+
+
+const router = new VueRouter({
+  routes: vueRoutes
+});
