861396c680f0ad03fb8a431c826463e710a6d948
jeferson <jefersongomes333@hotmail.com>
2020-12-20 10:24:01 -0300

    adcionando vue router

--- views/public/app.js
+++ views/public/app.js
@@ -1,11 +1,13 @@
 
 new Vue({
-    el: '#app',
+    router,
     components: {
         'navbar-component': navbarComponent,
         'cadastro-component': cadastroComponent,
         'login-component': loginComponent,
         'filtro-jobs-component': filtroJobsComponent,
         'list-jobs-component': listJobsComponent,
+        'perfil-descricao-component': perfilDescricaoComponent,
+        'perfil-component': perfilComponent,
     }
-})
+}).$mount('#app');
