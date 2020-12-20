830cb1a1fdad329296d5fc42a4f1f5b3222aafaf
jeferson <jefersongomes333@hotmail.com>
2020-12-20 13:33:17 -0300

    dados do perfil dinamicos, add prop avaliacao_usuario

--- views/components/perfil-descricao.js
+++ views/components/perfil-descricao.js
@@ -2,7 +2,16 @@ var perfilDescricaoComponent = {
     props: {
         ation_profile_img: {
             type: String,
-            default: "http://localhost/jobfinder/controlador/usuario_imagem.php"
+            required: true
+        },
+        avaliacao_usuario: { //aqui deve vir do banco a media das avaliações deste usuário
+            required: true
+        },
+    },
+    data(){
+        return{
+            usuario: user, //dado vem da session do usuario logado, variável está no footer do layout app
+            ava_usuario: parseInt(this.avaliacao_usuario) //criando uma nova variavel para converter o valor padrao para inteiro
         }
     },
     template: `
@@ -10,34 +19,30 @@ var perfilDescricaoComponent = {
             <div class="d-flex flex-column justify-content-center align-items-center p-2 mt-3">
                 <form id="change_profile_pic" :action="ation_profile_img" method="post">
                     <label for="img_profile">
-                        <img src="https://boostchiropractic.co.nz/wp-content/uploads/2016/09/default-user-img.jpg" alt="img profile user" id="perfil_img_user" />
+                        <img :src="usuario.foto_perfil" alt="img profile user" id="perfil_img_user" />
                         <div id="choice-file">
-                            <i class="fas fa-camera    "></i>
+                            <i class="fas fa-camera"></i>
                         </div>
                     </label>
                     <input class="d-none" type="file" id="img_profile" v-on:change="edit_profile_file" />
                 </form>
                 <span class="text-uppercase font-weight-bold text-center">
-                    Username
+                    {{usuario.apelido}}
                 </span>
             </div>
-            <p class="text-center" style="color: yellow">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
+            <p class="text-center">
+                <i class="fas fa-star" v-for="i in (0, 5)" :class="i < ava_usuario ? ' text-warning' : ' text-muted'"></i>
             </p>
             <p class="text-center mt-5">
-                <strong><i>email@email.com</i></strong>
+                <strong><i>{{usuario.email}}</i></strong>
             </p>
             <p class="text-center">
-                <strong><i>(87) 9 8800-0000</i></strong>
+                <strong><i>{{usuario.telefone}}</i></strong>
             </p>
         </div>
     `,
     methods: {
-        edit_profile_file(event) {
+        edit_profile_file() {
             let op = confirm("Deseja salvar essa imagem como foto de perfil?")
             if (op) {
                 $('#change_profile_pic').submit();
