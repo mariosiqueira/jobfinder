6a3009e8ddc0b7b397dcf728bc100356db958dc3
jeferson <jefersongomes333@hotmail.com>
2020-12-21 12:27:19 -0300

    estilo e validação - em construção

--- views/components/login.js
+++ views/components/login.js
@@ -14,18 +14,45 @@ var loginComponent = {
             type: String
         }
     },
+    data() {
+        return {
+            email: "",
+            senha: "",
+            emailValid: true,
+            senhaValid: true,
+        }
+    },
+    methods:{
+        validadeInput(campoValid){
+            console.log(campoValid);
+            // if(!this.campoValid){
+            //     this.campoValid = false;
+            // }
+            // console.log(campoValid);
+
+        }
+    },  
     template: `
     <div class="jf-container container-acesso justify-content-center">
         <div class="col-md-5 m-0 p-0 mx-auto">
-            <form :action="action_login" method="post" class="mx-auto p-5 padding-md rounded" style="background-color: lightgrey">
+            <form :action="action_login" method="post" class="mx-auto p-5 padding-md rounded shadow">
                 <div class="form-row">
                     <div class="col-md-12 form-group">
                         <label for="email" class="required">E-mail</label>
-                        <input type="text" class="form-control" placeholder="usuario@email.com">
+                        <input type="text" class="form-control" name="email" v-model="email" placeholder="usuario@email.com"
+                        @blur="validadeInput('this.emailValid')"
+                        :class="!email & !emailValid? ' is-invalid':''"
+                        >
+                        <small class="invalid-feedback">
+                            Este campo é obrigatório
+                        </small>
                     </div>
                     <div class="col-md-12 form-group">
                         <label for="senha" class="required">Senha</label>
-                        <input type="password" class="form-control" placeholder="**********">
+                        <input type="password" name="senha" v-model="senha" class="form-control" placeholder="**********">
+                        <small class="invalid-feedback">
+                            Este campo é obrigatório
+                        </small>
                     </div>
                 </div>
                 <div class="form-check form-check-inline">
@@ -35,9 +62,15 @@ var loginComponent = {
                     </label>
                 </div>
                 <div class="form-group mt-5">
-                    <button type="submit" class="btn btn-md btn-primary btn-block rounded-pill">Entrar</button>
-                    <a class="btn btn-md btn-light btn-block rounded-pill"
-                        :href="url_cadastro">Cadastre-se</a>
+                    <button type="submit" class="btn btn-primary font-weight-bold btn-md btn-primary btn-block rounded-pill" v-if="senha && email">
+                        Entrar
+                    </button>
+                    <button type="button" class="btn btn-primary font-weight-bold btn-md btn-primary btn-block rounded-pill" v-else disabled>
+                        Entrar
+                    </button>
+                    <a class="btn btn-md btn-light btn-block rounded-pill":href="url_cadastro">
+                        Cadastre-se
+                    </a>
                 </div>
             </form>
         </div>
