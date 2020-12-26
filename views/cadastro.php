65009f7f5138ab4053ef3c8d166a72ebbefa3157
jeferson <jefersongomes333@hotmail.com>
2020-12-26 16:14:05 -0300

    add autenticacao

--- views/cadastro.php
+++ views/cadastro.php
@@ -1,3 +1,8 @@
+<?php
+    if (auth()) {
+        header("location: $routes->perfil");
+    }
+?>
 <?php require "layouts/app/head.php"?>
     <cadastro-component url_cadastro="<?php echo $routes->cadastro;?>" url_login='<?php echo $routes->login;?>'></cadastro-component>
 <?php require "layouts/app/footer.php"?>
