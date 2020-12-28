9aeecc7fa41fe870d063512cd857857cab0d85c4
jeferson <jefersongomes333@hotmail.com>
2020-12-28 12:41:29 -0300

    adicionando modal para termos e responsabilidade e deletar conta

--- views/perfil.php
+++ views/perfil.php
@@ -8,78 +8,84 @@
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"aberto"          
         ],
         [
             "id"=>2,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"aberto"          
         ],
         [
             "id"=>3,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"aberto"          
         ],
         [
             "id"=>4,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"aberto"          
         ],
         [
             "id"=>5,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"finalizado"          
         ],
         [
             "id"=>6,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"finalizado"          
         ],
         [
             "id"=>7,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"finalizado"          
         ],
         [
             "id"=>8,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"aberto"          
         ],
         [
             "id"=>9,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"aberto"          
         ],
         [
             "id"=>10,
             "titulo"=> "servico teste",
             "descricao"=>"servico teste de teste categoria",
             "endereco"=>"afogados da ingazeira, centro, 21",
-            "valor"=>"800.00",           
+            "valor"=>"800.00", 
+            "status"=>"finalizado"          
         ],
     ];
     $datactt = [
-        [
-            "id"=>1,
-            "nome"=> "username",
-            "foto_perfil"=>"https://boostchiropractic.co.nz/wp-content/uploads/2016/09/default-user-img.jpg",
-        ],
+        
         [
             "id"=>2,
             "nome"=> "username",
@@ -99,43 +105,60 @@
             "id"=>5,
             "nome"=> "username",
             "foto_perfil"=>"https://boostchiropractic.co.nz/wp-content/uploads/2016/09/default-user-img.jpg",
-        ]
+        ],
+        [
+            "id"=>6,
+            "nome"=> "username",
+            "foto_perfil"=>"https://boostchiropractic.co.nz/wp-content/uploads/2016/09/default-user-img.jpg",
+        ],
     ];
     $datamsg = [
         [
-            "id"=>1,
-            "to"=>1,
-            "from"=>2,
+            "id"=>11,
+            "contratado_id"=>1,
+            "contratante_id"=>2,
             "mensagem"=>"olá"
         ],
         [
-            "id"=>2,
-            "to"=>2,
-            "from"=>1,
+            "id"=>22,
+            "contratado_id"=>1,
+            "contratante_id"=>2,
+            "mensagem"=>"olá"
+        ],
+        [
+            "id"=>33,
+            "contratado_id"=>1,
+            "contratante_id"=>2,
+            "mensagem"=>"olá"
+        ],
+        [
+            "id"=>24,
+            "contratado_id"=>2,
+            "contratante_id"=>1,
             "mensagem"=>"olá tudo bem?"
         ],
         [
             "id"=>3,
-            "to"=>1,
-            "from"=>3,
+            "contratado_id"=>1,
+            "contratante_id"=>3,
             "mensagem"=>"olá tudo bem?"
         ],
         [
             "id"=>4,
-            "to"=>3,
-            "from"=>1,
+            "contratado_id"=>3,
+            "contratante_id"=>1,
             "mensagem"=>"olá tudo bem? olá tudo bem? olá tudo bem? olá tudo bem? olá tudo bem? olá tudo bem?"
         ],
         [
             "id"=>5,
-            "to"=>1,
-            "from"=>3,
+            "contratado_id"=>1,
+            "contratante_id"=>3,
             "mensagem"=>"olá tudo bem?"
         ],
         [
             "id"=>6,
-            "to"=>3,
-            "from"=>1,
+            "contratado_id"=>3,
+            "contratante_id"=>1,
             "mensagem"=>"olá tudo bem?"
         ],
     ];
@@ -178,6 +201,48 @@
                         </button>
                     </div>
                 </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal termos e responsabilidade -->
+<div class="modal fade" id="termos_e_responsabilidade" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h3 class="text-muted text-uppercase text-center mb-4">Termos e resonsabilidade</h3>
+                <div class="col-md-6 mx-auto form-group text-center">
+                    <p class="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur aspernatur
+                        tempore alias sed, exercitationem sapiente doloremque aliquam magnam, nihil ipsam incidunt
+                        dolore voluptates necessitatibus eius beatae itaque, a optio!
+                    </p>
+                    <p class="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur aspernatur
+                        tempore alias sed, exercitationem sapiente doloremque aliquam magnam, nihil ipsam incidunt
+                        dolore voluptates necessitatibus eius beatae itaque, a optio!
+                    </p>
+                    <p class="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur aspernatur
+                        tempore alias sed, exercitationem sapiente doloremque aliquam magnam, nihil ipsam incidunt
+                        dolore voluptates necessitatibus eius beatae itaque, a optio!
+                    </p>
+                    <p class="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur aspernatur
+                        tempore alias sed, exercitationem sapiente doloremque aliquam magnam, nihil ipsam incidunt
+                        dolore voluptates necessitatibus eius beatae itaque, a optio!
+                    </p>
+                    <p class="text-right">
+                        <strong>JOBFINDER</strong>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
@@ -227,6 +292,33 @@
                         </button>
                     </div>
                 </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="deletar_conta" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+    aria-hidden="true">
+    <div class="modal-dialog  modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h4 class="text-muted text-uppercase text-center">Deseja realmente deletar sua conta?</h4>
+                <form action="urldeletarconta" method="post" id="deletarConta">
+                    <div class="form-group mt">
+                        <span class="badge badge-danger p-2 mt-5 mb-2">Para deletar sua conta, você precisa confirmar sua senha</span>
+                        <input type="password" class="form-control shadow-none" name="senha" placeholder="Senha" id="inputDeletarConta" required />
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary shadow-none" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-danger shadow-none" id="btnDeletarConta"               onclick="$('#deletarConta').submit()">Deletar</button>
             </div>
         </div>
     </div>
