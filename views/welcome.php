abac364ea5915ac635c53e3fe1919f7e6c4537a2
jeferson <jefersongomes333@hotmail.com>
2020-12-28 12:45:12 -0300

    adicionando fonts

--- views/welcome.php
+++ views/welcome.php
@@ -3,7 +3,7 @@
     <div class="position-absolute w-75">
         <div class="d-flex m-0 justify-content-center">
             <div class="text-center">
-                <h1 class="text-white text-uppercase text-center mb-5">
+                <h1 class="text-white open-sans text-uppercase text-center mb-5">
                     Seja Freelancer
                 </h1>
                 <a class="btn btn-light btn-md text-uppercase my-5" href="<?php echo $routes->jobs;?>">
@@ -15,8 +15,8 @@
     </div>
 </header>
 <main class="padding-md">
-    <h1 class="text-muted text-uppercase text-center">JobFinder</h1>
-    <hr class="w-25" style="background-color: DarkSlateGray; height: 2px">
+    <h1 class="text-muted open-sans text-uppercase text-center">JobFinder</h1>
+    <hr class="w-25 mx-auto" style="background-color: DarkSlateGray; height: 2px">
     <h5 class="text-muted text-center w-10 mx-auto my-5"><strong>JOBFINDER</strong> é uma plataforma de compartilhamento
         de serviços curtos - freelas - e contratação de profissionais para tais.<br> Faça seu <a
             href="<?php echo $routes->perfil?>">cadastro</a> e começe a usar.</h5>
@@ -80,8 +80,8 @@
 
 </main>
 <section class="container-item-details padding-md">
-    <h1 class="text-white text-uppercase text-center mt-5" style="font-size:max(2.5vw, 18pt)">Funcionalidades</h1>
-    <hr class="w-25" style="background-color: white; height: 2px">
+    <h1 class="text-white open-sans text-uppercase text-center mt-5" style="font-size:max(2.5vw, 18pt)">Funcionalidades</h1>
+    <hr class="w-25 mx-auto" style="background-color: white; height: 2px">
     <h5 class="text-white text-center w-10 mx-auto my-5">
         Veja abaixo as principais funcionalidades que o <strong>JOBFINDER</strong> oferece a você.
     </h5>
@@ -132,12 +132,12 @@
     <div class="d-flex justify-content-around flex-wrap p-5 padding-md">
         <div class="d-flex flex-column">
             <img class="hero-footer" src="<?php echo $routes->home.'views/public/img/logomarca.png'?>" alt="" srcset="">
-            <h1 class="text-muted text-uppercase">JobFinder</h1>
+            <h1 class="text-muted text-uppercase open-sans">JobFinder</h1>
         </div>
         <div class="d-flex flex-column">
-            <h5 class="text-muted text-uppercase">Nossas redes sociais</h5>
-            <span>Nos siga, para mais informações</span>
-            <p class="text-muted mt-5">
+            <h5 class="text-muted open-sans text-uppercase">Nossas redes sociais</h5>
+            <span class="open-sans">Nos siga, para mais informações</span>
+            <p class="text-muted open-sans mt-5">
                 <span>
                     <i class="fab fa-facebook" aria-hidden="true"></i>
                     Facebook
@@ -153,9 +153,9 @@
             </p>
         </div>
         <div class="d-flex flex-column">
-            <h5 class="text-muted text-uppercase">Endereço</h5>
-            <span>Rua 1, Centro - Afogados da Ingazeira/PE</span>
-            <p class="mt-5">
+            <h5 class="text-muted text-uppercase open-sans">Endereço</h5>
+            <span class="open-sans">Rua 1, Centro - Afogados da Ingazeira/PE</span>
+            <p class="mt-5 open-sans">
                 <a href="<?php echo $routes->login?>">
                     <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     Login
@@ -173,7 +173,7 @@
                     Serviços
                 </a><br>
             </p>
-            <p>
+            <p class="open-sans">
                 <a href="">
                     <i class="fas fa-file-alt    "></i>
                     Termos e serviço
@@ -186,7 +186,7 @@
         </div>
     </div>
     <div class="d-flex justify-content-center m-0 mt-5">
-        <p class="text-center">
+        <p class="text-center open-sans">
             <?php echo date('Y')?> &copy; copyright todos os direitos reservados
         </p>
     </div>
