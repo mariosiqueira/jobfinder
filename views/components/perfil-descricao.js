1dfe23e9169f4b1ae4de32a0cd0cc4083e160bd1
Mário Sérgio dos Santos Siqueira <mario.siqueiira@gmail.com>
2020-12-29 15:36:47 -0300

    Ajuste de for das estrelas

--- views/components/perfil-descricao.js
+++ views/components/perfil-descricao.js
@@ -9,7 +9,7 @@ var perfilDescricaoComponent = {
         },
         url:{
             required:true
-        }
+        },
     },
     data(){
         return{
@@ -35,7 +35,7 @@ var perfilDescricaoComponent = {
                 </span>
             </div>
             <p class="text-center">
-                <i class="fas fa-star" v-for="i in (0, 5)" :class="i < ava_usuario ? ' text-warning' : ' text-muted'"></i>
+                <i class="fas fa-star" v-for="i in (0, 5)" :class="i <= ava_usuario ? ' text-warning' : ' text-muted'"></i>
             </p>
             <p class="text-center mt-5">
                 <strong><i>{{usuario.email}}</i></strong>
