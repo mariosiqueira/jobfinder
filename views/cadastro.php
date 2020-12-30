012fa3e0fac7c2181aa4e419bc3ad0a4b52a1998
jeferson <jefersongomes333@hotmail.com>
2020-12-30 12:13:21 -0300

    adicionando props de cadastro

--- views/cadastro.php
+++ views/cadastro.php
@@ -4,5 +4,5 @@
     }
 ?>
 <?php require "layouts/app/head.php"?>
-    <cadastro-component url_cadastro="<?php echo $routes->cadastro;?>" url_login='<?php echo $routes->login;?>'></cadastro-component>
+    <cadastro-component url_cadastro="<?php echo $routes->cadastro;?>" url_login='<?php echo $routes->login;?>' action_cadastro = '<?php echo $routes->action_cadastro; ?>'></cadastro-component>
 <?php require "layouts/app/footer.php"?>
