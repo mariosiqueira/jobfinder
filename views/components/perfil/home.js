1edfc4c73a14b55e1f6065dd60012b922f15cbd3
jeferson <jefersongomes333@hotmail.com>
2020-12-30 12:17:56 -0300

    adicionadno descrição do usuário no perfil

--- views/components/perfil/home.js
+++ views/components/perfil/home.js
@@ -3,7 +3,22 @@ var HomeComponent = {
     template: `
     <div>
         <h1 class="text-muted text-uppercase">Olá, {{user.apelido}}.</h1>
-        <div class="my-4"></div>
+        <div class="my-5">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-success mr-4">
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                    {{user.nome}}
+                </li>
+                <li class="list-group-item list-group-item-success mr-4">
+                    <i class="fas fa-envelope    "></i>
+                    {{user.email}}
+                </li>
+                <li class="list-group-item list-group-item-success mr-4">
+                    <i class="fas fa-phone    "></i>
+                    {{user.telefone}}
+                </li>
+            </ul>
+        </div>
     </div>
     `,
     data() {
@@ -16,6 +31,4 @@ var HomeComponent = {
             alert('Indisponível');
         },
     },
-    mounted() {
-    }
 }
