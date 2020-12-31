30731930f8a22f5d07827849e36163a79051e274
jeferson <jefersongomes333@hotmail.com>
2020-12-31 12:17:49 -0300

    home url prop

--- views/public/router.js
+++ views/public/router.js
@@ -3,7 +3,7 @@ const vueRoutes = [
   { path: '/services', name: 'services', component: JobComponent, props: route => ({ servicos: route.query.servicos }) },
   { path: '/services/show/:id', name: 'services_show', component: servicoShow, props: route => ({ servicos: route.params.id }) },
   { path: '/services/close/:id', name: 'services_close', component: servicoClose, props: route => ({ servicos: route.params.id }) },
-  { path: '/messages', name: 'messages', component: MessagesComponent, props: route => ({ mensagens: route.query.mensagens, contatos: route.query.contatos }) },
+  { path: '/messages', name: 'messages', component: MessagesComponent, props: route => ({ mensagens: route.query.mensagens, contatos: route.query.contatos, homeurl: route.query.homeurl }) },
   { path: '/configuration', component: ConfigComponent },
   { path: '/rating', name:'rating', component: AvaliacoesComponent, props: route => ({ avaliacoes: route.query.avaliacoes }) },
 ];
