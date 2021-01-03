ad900417ec1922866aa154f45b16d8c6247a0bfd
jeferson <jefersongomes333@hotmail.com>
2021-01-03 12:22:41 -0300

    salvando as mensagens no banco de dados

--- views/components/perfil/mensagens.js
+++ views/components/perfil/mensagens.js
@@ -77,7 +77,7 @@ var MessagesComponent = {
     methods: {
         loadMensagens(id) {
             this.focused = true;
-            this.from = id; 
+            this.from = id;
             let aux = [];
             this.msgs.forEach(e => { //percore todas as mensagens do usuario logado
                 if (e.contratante_id == id || e.contratado_id == id) { //pega so as mensagens do usuario logado e do contato que ele clicou
@@ -89,7 +89,7 @@ var MessagesComponent = {
             this.$refs.mensagem.focus(); //da focus no campo de input de mensagem
 
         },
-        sendMensagem() { //função pra o usuario enviar a mensagem
+        async sendMensagem() { //função pra o usuario enviar a mensagem
             if (this.from != "") { //verifica se o usuario de destino da mensagem nao esta vazio
 
                 if (this.mensagem && this.mensagem != "") { //verifica se a mensagem não está vazia
@@ -101,6 +101,12 @@ var MessagesComponent = {
                         "sala": this.from //sala é o id do ususario da mensagem de destino, ou seja so o usuario com id igual ao do contratado_id podera visualiza-la
                     };
                     this.data.push(aux); //adiciona a mensagem enviada ao array de todas as mensagens
+
+                    this.axiosSend({
+                        'contratante_id': this.user_id,
+                        'contratado_id': this.from,
+                        'mensagem': this.mensagem
+                    })
 
                     this.send(aux); //socket envia a mensagem
 
@@ -121,7 +127,7 @@ var MessagesComponent = {
         },
         connect() {
             this.conn = new WebSocket('ws://localhost:8180'); //cria uma conexao socket
-            this.conn.onopen = async (e) =>  await console.log(''); //função de coneção aberta
+            this.conn.onopen = async (e) => await console.log(''); //função de coneção aberta
             this.conn.onmessage = (e) => this.salvarMensagem(e.data); //toda mensagem  recebida pelo socket
         },
         send(data) { //função de enviar a mensagem pelo socket
@@ -135,12 +141,16 @@ var MessagesComponent = {
                 return;
             }
             this.conn.send(JSON.stringify(data)); //enviando pelo socket 
-            
-        },
-        salvarMensagem(msg){ //função pra quando uma nova mensagem é recebida no metodo onmessage.
-            var data = JSON.parse(msg); 
 
-            if(data.sala == this.user_id){ //verifica se a mensagem recebida tem o id da sala igual ao id do usuario logado
+        },
+        async axiosSend(data) {
+            var res = await axios.post(this.homeurl + 'controller/action_salvar_mensagem.php', data);
+            console.log(res);
+        },
+        salvarMensagem(msg) { //função pra quando uma nova mensagem é recebida no metodo onmessage.
+            var data = JSON.parse(msg);
+
+            if (data.sala == this.user_id) { //verifica se a mensagem recebida tem o id da sala igual ao id do usuario logado
                 if (this.focused) { //verifica se ele ta com o campode de mensagem aberta
                     this.msgs.push(data); //adiciona a todas as mesagens
                     this.data.push(data); //adiciona a mensagem a tela pra ele ver
