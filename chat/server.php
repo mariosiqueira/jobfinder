c9eb6a9a11953f6469220370d9b646b6e5130adb
jeferson <jefersongomes333@hotmail.com>
2021-01-08 20:33:37 -0300

    Update server.php

--- chat/server.php
+++ chat/server.php
@@ -1,7 +1,7 @@
 <?php
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-
+require $_SERVER['DOCUMENT_ROOT']. '/vendor/autoload.php';
+var_dump($_SERVER['DOCUMENT_ROOT']);
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
