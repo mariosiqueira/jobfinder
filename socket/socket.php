31fcbcfa643bab16cf6954cf86062511d04fabe1
jeferson <jefersongomes333@hotmail.com>
2021-01-08 21:38:24 -0300

    Update socket.php

--- socket/socket.php
+++ socket/socket.php
@@ -1,7 +1,7 @@
 <?php
-require "./vendor/autoload.php";
-require "chat.php";
-// require_once(dirname(__DIR__) . '/vendor/autoload.php');
+// require "./vendor/autoload.php";
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
+require "./chat/chat.php";
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
