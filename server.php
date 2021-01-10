b93cd749fb5787369312e39413e60d5fbe9b2995
jeferson <jefersongomes333@hotmail.com>
2021-01-10 20:56:41 -0300

    Update server.php

--- server.php
+++ server.php
@@ -1,10 +1,10 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
+
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
-use MyApp\Socket\Chat;
-require __DIR__.'/src/socket/Chat.php';
+use MyApp\Chat;
 
 $server = IoServer::factory(
     new HttpServer(
