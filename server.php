01d77ec48c8c9b42f1b45f0a896aad280ce8e152
jeferson <jefersongomes333@hotmail.com>
2021-01-10 20:47:07 -0300

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__DIR__) . '/vendor/autoload.php');
+require_once(dirname(__DIR__) . 'vendor/autoload.php');
 
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
