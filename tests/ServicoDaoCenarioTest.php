5976325d48d1ad82336b27f98168bd806a99e2f4
Mário Sérgio dos Santos Siqueira <mario.siqueiira@gmail.com>
2021-01-11 20:15:10 -0300

    Sincronização de testes

--- tests/ServicoDaoCenarioTest.php
+++ tests/ServicoDaoCenarioTest.php
@@ -6,7 +6,26 @@ use App\Tests\ServicoDaoCenario;
 
 final class ServicoDaoCenarioTest extends TestCase
 {
-    public function testSalvatServico(): void
+    public function testSalvaServico(): void
+    {
+        // Método usando para pular esse test
+        $this->markTestIncomplete(
+            'Ignorando o test de salvar serviço para nao persistir no banco.'
+        );
+
+        $servico = new Servico();
+        $servico->setTitulo('servico test');
+        $servico->setDescricao('descrição serviço teste');
+        $servico->setEnderecoServico('endereço teste');
+        $servico->setValor(100.53);
+        $servico->setStatus("aberto");
+        $servico->setUsuarioId(1); //id de um usuario cadastrado no banco
+
+        $dao = new ServicoDaoCenario();
+        $this->assertNotNull($dao->cadastrar($servico)); //É esperado que o serviço seja criado com sucesso
+    }
+
+    public function testSalvaServicoErro(): void
     {
         $servico = new Servico();
         $servico->setTitulo('servico test');
