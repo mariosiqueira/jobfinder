f415a3cb9817930b16bacf6369113c39b8d77682
jeferson <jefersongomes333@hotmail.com>
2021-01-11 19:46:27 -0300

    teste de cadastro quando o e-mail já existe no banco e função pra pular o teste

--- tests/UsuarioDaoCenarioTest.php
+++ tests/UsuarioDaoCenarioTest.php
@@ -8,16 +8,35 @@ final class UsuarioDaoCenarioTest extends TestCase
 {
     public function testFazerCadastro(): void
     {
+        // Método usando para pular esse test
+        $this->markTestIncomplete(
+            'Ignorando o test de salvar usuário para nao persistir no banco.'
+        );
+
         $usuario = new Usuario();
         $usuario->setNome('usuario test');
         $usuario->setApelido('usuario');
         $usuario->setTelefone('(00) 0 0000-0000');
-        $usuario->setEmail('usuario@email.com');
+        $usuario->setEmail('usuario@email.com'); //o e-mail deve ser unique
         $usuario->setSenha(md5('12345'));
         $usuario->setFotoPerfil('default-user-img.jpg');
 
         $dao = new UsuarioDaoCenario();
         $this->assertNotNull($dao->cadastrar($usuario)); //É esperado que nao retorne null
+    }
+
+    public function testFazerCadastroEmailExistente(): void
+    {
+        $usuario = new Usuario();
+        $usuario->setNome('usuario test');
+        $usuario->setApelido('usuario');
+        $usuario->setTelefone('(00) 0 0000-0000');
+        $usuario->setEmail('usuario@email.com'); //usando e-mail já existente
+        $usuario->setSenha(md5('12345'));
+        $usuario->setFotoPerfil('default-user-img.jpg');
+
+        $dao = new UsuarioDaoCenario();
+        $this->assertNull($dao->cadastrar($usuario)); //É esperado que retorne null porque o e-mail já está cadastrado
     }
 
     public function testLogarCorretamente(): void
