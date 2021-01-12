4cdced2c8a326349237cf369860945dac461c451
jeferson <jefersongomes333@hotmail.com>
2021-01-12 15:50:56 -0300

    sincronização

--- src/tests/UsuarioDaoCenario.php
+++ src/tests/UsuarioDaoCenario.php
@@ -34,4 +34,47 @@ class UsuarioDaoCenario
             return null;
         } 
     }
+
+    public function buscarUsuarioPeloEmail($email) 
+    {
+        $usuarioDaoMysql = new UsuarioDaoMysql($this->pdo);
+
+        $aux = $usuarioDaoMysql->buscarPeloEmail($email);
+
+        return $aux == false ? null : $aux;
+    }
+
+    public function buscarUsuarioPeloId($id) 
+    {
+        $usuarioDaoMysql = new UsuarioDaoMysql($this->pdo);
+
+        $aux = $usuarioDaoMysql->buscarPeloId($id);
+
+        return $aux == false ? null : $aux;
+    }
+
+    public function atualizar(Usuario $usuario) 
+    {
+        $usuarioDaoMysql = new UsuarioDaoMysql($this->pdo);
+        $aux = $usuarioDaoMysql->buscarPeloId($usuario->getId());
+        if($aux == false) {
+            return false;
+        }
+
+        return $usuarioDaoMysql->atualizar($usuario);
+    }
+
+    public function atualizarApelido($id, $apelido) 
+    {
+        $usuarioDaoMysql = new UsuarioDaoMysql($this->pdo);
+        $aux = $usuarioDaoMysql->buscarPeloId($id);
+        if($aux == false) {
+            return false;
+        }
+        if($apelido) {
+            $aux->setApelido($apelido);
+            return $usuarioDaoMysql->atualizar($aux);
+        }
+        return false;
+    }
 }
