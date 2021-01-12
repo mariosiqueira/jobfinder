b4c60a5843cde6c4460f6aeac057aeeb56fb4fb7
jeferson <jefersongomes333@hotmail.com>
2021-01-12 15:43:15 -0300

    Update ServicoDaoCenarioTest.php

--- tests/ServicoDaoCenarioTest.php
+++ tests/ServicoDaoCenarioTest.php
@@ -38,4 +38,59 @@ final class ServicoDaoCenarioTest extends TestCase
         $dao = new ServicoDaoCenario();
         $this->assertNull($dao->cadastrar($servico)); //É esperado null pq não foi passado o id do usuario que criou o serviço 
     }
+
+    public function testDelatarServico() :void
+    {
+        // Método usando para pular esse test
+        $this->markTestIncomplete(
+            'Ignorando o test de deletar serviço para nao persistir no banco.'
+        );
+        $dao = new ServicoDaoCenario();
+
+        $this->assertTrue($dao->deletar(1)); // é esperado o retorno true ao deletar um serviço válido
+    }
+
+    public function testDelatarServicoInexistente() :void
+    {
+        $dao = new ServicoDaoCenario();
+
+        //foi utilizado um id de um serviço que não existe no banco de dados.
+        $idInvalido = 100;
+
+        $this->assertNotTrue($dao->deletar($idInvalido)); // é esperado o retorno diferente de true ao deletar um serviço inválido.
+    }
+
+    public function testUpdateServico() :void
+    {
+        $dao = new ServicoDaoCenario();
+
+        //id de um serviço válido do banco 
+        $idServico = 1;
+
+        // criando novos dados pra editar o serviço
+        $novosDados = array(
+            "titulo" => "Novo titulo update teste 1",
+            "descricao" => "Nova descrição update teste 1",
+            "enderecoServico" => "Novo endereço update teste 1",
+        );
+
+        $this->assertTrue($dao->atualizar($idServico, $novosDados)); // é esperado o retorno de true ao atualizar o serviço.
+    }
+
+    public function testUpdateServicoInexistente() :void
+    {
+        $dao = new ServicoDaoCenario();
+
+        //id de um serviço inválido do banco 
+        $idServico = 100;
+
+        // criando novos dados pra editar o serviço
+        $novosDados = array(
+            "titulo" => "Novo titulo update teste 2",
+            "descricao" => "Nova descrição update teste 2",
+            "enderecoServico" => "Novo endereço update teste 2",
+        );
+
+        $this->assertNotTrue($dao->atualizar($idServico, $novosDados)); // é esperado o retorno diferente de true ao atualizar o serviço com id inválido.
+    }
 }
