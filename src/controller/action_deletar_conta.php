fe4c430927ca2a4ba01134c26a3528f145c1c03a
Mário Sérgio dos Santos Siqueira <mario.siqueiira@gmail.com>
2021-01-13 11:48:36 -0300

    Simplificação da funcionalidade de deletar conta de usuário

--- src/controller/action_deletar_conta.php
+++ src/controller/action_deletar_conta.php
@@ -2,68 +2,38 @@
 use App\Config\Conexao;//importa o PDO para utilizar na classe UsuarioDaoMysql
 use App\Dao\UsuarioDaoMysql; //Deleta o usuário do banco de dados
 use App\Dao\ServicoDaoMysql; //Deleta os serviços do usuário no banco de dados
-use App\Dao\ServicoCategoriaDaoMysql; //Deleta as categorias associadas aos serviços do usuário no banco de dados
 use App\Dao\UsuarioServicoDaoMysql; //Deleta a associação entre um usuário e um serviço
-use App\Dao\AvaliacaoDaoMysql; // Deleta as avaliações feitas ao/pelo usuário que vai ser removido
-use App\Dao\MensagemDaoMysql; //Apaga as ocorrências de mensagens que foram trocadas por um usuário que vai ser removido
 
 $pdo = Conexao::getInstance();
 
 $usuarioDao = new UsuarioDaoMysql($pdo);
 $servicoDao = new ServicoDaoMysql($pdo);
-$servicoCategoriaDao = new ServicoCategoriaDaoMysql($pdo);
 $usuarioServicoDao = new UsuarioServicoDaoMysql($pdo);
-$avaliacaoDao = new AvaliacaoDaoMysql($pdo);
-$mensagemDao = new MensagemDaoMysql($pdo);
 
-// #################### Funções auxiliares à função principal deletarUsuario ####################
-function deletarCategoriasAssociadasAUmServico($servicoCategoriaDao, $arrayServicos) {
-    foreach($arrayServicos as $servico) {
-        $servicoCategoriaDao->deletar($servico->getId());
-    }
-}
+// #################### Funções auxiliar à função principal deletarUsuario ####################
 
-function deletarTodasAssociacoesUsuarioServico($usuarioServicoDao, $arrayServicos) {
-    foreach($arrayServicos as $servico) {
-        $usuarioServicoDao->deletarPeloServicoId($servico->getId());
-    }
-}
-
-function deletarAvaliacoesDoUsuario($avaliacaoDao, $id) {
-    $avaliacaoDao->deletarAvaliacaoPeloUsuarioId($id);
-    $avaliacaoDao->deletarAvaliacaoPeloAvaliadorId($id);
-}
-
-function deletarMensagensDoUsuario($mensagemDao, $id) {
-    $mensagemDao->deletarMensagemPeloContratanteId($id);
-    $mensagemDao->deletarMensagemPeloContratadoId($id);
-}
-
-function deletarServicos($servicoDao, $arrayServicos) {
-    foreach($arrayServicos as $servico) {
-        $servicoDao->deletar($servico->getId());
-    }
-}
-
-//Método exclusivo para o caso do usuário que será excluído não ter cadastrado serviço, mas apenas se candidatado.
+//Método exclusivo para o caso do usuário que será excluído ter se candidatado em algum serviço.
 function deletarContratadoDeServicos($usuarioServicoDao, $contratadoId) {
     $arrayUsuarioServicos = $usuarioServicoDao->buscarPeloContratadoId($contratadoId); //Vai receber todos os objetos de usuarioServico para o contratado ser desassociado do servico.
-    foreach($arrayUsuarioServicos as $usuarioServico) {
-        $usuarioServico->setContratadoId(null); //Ao invés de deletar por completo esse serviço, apenas atualizamos o id do contratado para null, pois este terá sua conta excluída.
-        $usuarioServicoDao->atualizar($usuarioServico);
+    if(!is_null($arrayUsuarioServicos)) {
+        foreach($arrayUsuarioServicos as $usuarioServico) {
+            $usuarioServico->setContratadoId(null); //Ao invés de deletar por completo esse serviço, apenas atualizamos o id do contratado para null, pois este terá sua conta excluída.
+            $usuarioServicoDao->atualizar($usuarioServico);
+        }
+        return true;
     }
+    return false;
 }
 
-// #################### Funções auxiliares à função principal deletarUsuario ####################
+// #################### Funções auxiliar à função principal deletarUsuario ####################
 
 /**
  * A função a seguir checa se usuário digitou uma senha e verifica se a senha digitada condiz com a senha do id dele.
- * Depois é necessário apagar todas as categorias associadas aos serviços que o usuário cadastrou bem como o serviço
- * cadastrado por ele. Depois serão apagados todas as associações de empregos que o usuário se interessou por trabalhar.
- * Serão apagadas todas as mensagens trocadas por esse usuário e, por fim, o próprio usuário será removido. 
+ * Em seguida é feito uma verificação se o usuário se candidatou a algum serviço e apaga a coluna de contratado_id da tabela
+ * usuario_servico. Por fim, o usuário é apagado e todas as suas relações com outras tabelas também. 
  **/
 
-function deletarUsuario($usuarioDao, $servicoDao, $servicoCategoriaDao, $usuarioServicoDao, $avaliacaoDao, $mensagemDao) {
+function deletarUsuario($usuarioDao, $servicoDao, $usuarioServicoDao) {
     $senha = md5(filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING));
     $usuario = unserialize($_SESSION['auth']);
     $usuario = $usuarioDao->buscarPeloId($usuario->getId());
@@ -71,23 +41,10 @@ function deletarUsuario($usuarioDao, $servicoDao, $servicoCategoriaDao, $usuario
     $deletou = false;
 
     if($senha && $senha == $usuario->getSenha()) {
-        $arrayServicos = $servicoDao->buscarServicoPeloIdDoUsuario($usuario->getId());
 
-        if($arrayServicos == null) { //Se o usuário não cadastrou serviço então não é preciso deletar as categorias e o serviço que ele se candidatou.
-            deletarContratadoDeServicos($usuarioServicoDao, $usuario->getId());
-            deletarAvaliacoesDoUsuario($avaliacaoDao, $usuario->getId());
-            deletarMensagensDoUsuario($mensagemDao, $usuario->getId());
-            $deletou = $usuarioDao->deletar($usuario->getId());
-            
-        } else {
-            deletarCategoriasAssociadasAUmServico($servicoCategoriaDao, $arrayServicos);
-            deletarTodasAssociacoesUsuarioServico($usuarioServicoDao, $arrayServicos);
-            deletarAvaliacoesDoUsuario($avaliacaoDao, $usuario->getId());
-            deletarMensagensDoUsuario($mensagemDao, $usuario->getId());
-            deletarServicos($servicoDao, $arrayServicos);
-            $deletou = $usuarioDao->deletar($usuario->getId());
+        deletarContratadoDeServicos($usuarioServicoDao, $usuario->getId());
+        $deletou = $usuarioDao->deletar($usuario->getId());
 
-        }
         //Removendo uma foto que não seja a default-user-img.jpg
         if($usuario->getFotoPerfil() != "default-user-img.jpg") {
             unlink($_SERVER['DOCUMENT_ROOT'].'/jobfinder/src/files/'.$usuario->getFotoPerfil());
@@ -121,4 +78,4 @@ function deletarUsuario($usuarioDao, $servicoDao, $servicoCategoriaDao, $usuario
     }
 }
 
-deletarUsuario($usuarioDao, $servicoDao, $servicoCategoriaDao, $usuarioServicoDao, $avaliacaoDao, $mensagemDao);
+deletarUsuario($usuarioDao, $servicoDao, $usuarioServicoDao);
