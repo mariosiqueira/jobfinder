e4f8b724be6f8a6db33468b57215f5e4da0d52f5
jeferson <jefersongomes333@hotmail.com>
2021-01-15 19:52:37 -0300

    funções para filtro

--- src/views/jobs.php
+++ src/views/jobs.php
@@ -1,48 +1,88 @@
 
 <?php require "layouts/app/head.php";
-use App\Config\Conexao; 
-use App\Dao\ServicoDaoMysql; 
-use App\Dao\UsuarioDaoMysql; 
+    use App\Config\Conexao; 
+    use App\Dao\ServicoDaoMysql; 
+    use App\Dao\ServicoCategoriaDaoMysql; 
+    use App\Dao\UsuarioDaoMysql; 
+    use App\Dao\CategoriaDaoMysql; 
 
-use App\VO\Servico;
+    use App\VO\Servico;
+    use App\VO\Categoria;
 
-$pdo = Conexao::getInstance();
+    $pdo = Conexao::getInstance();
 
-$usuarioDao = new UsuarioDaoMysql($pdo);
-$servicoDao = new ServicoDaoMysql($pdo);
-$servicos = [];
-
-$arrayDadosObjetosServico = $servicoDao->buscarTodos();
-if($arrayDadosObjetosServico != null) {
+    $usuarioDao = new UsuarioDaoMysql($pdo);
+    $servicoDao = new ServicoDaoMysql($pdo);
+    $servicoCategoriaDao = new ServicoCategoriaDaoMysql($pdo);
+    $servicos = [];
     
-    foreach($arrayDadosObjetosServico as $dadoObjetoServico) {
-        if(auth()){
-            if($dadoObjetoServico->getUsuarioId() != getUser()->getId()){
-                $novoServico = new Servico();
-                $novoServico->setId($dadoObjetoServico->getId());
-                $novoServico->setTitulo($dadoObjetoServico->getTitulo());
-                $novoServico->setDescricao($dadoObjetoServico->getDescricao());
-                $novoServico->setEnderecoServico($dadoObjetoServico->getEnderecoServico());
-                $novoServico->setValor($dadoObjetoServico->getValor());
-                $novoServico->setUsuarioId($usuarioDao->buscarPeloId($dadoObjetoServico->getUsuarioId()));
-                $novoServico->setDataPostagem($dadoObjetoServico->getDataPostagem());
-                $novoServico->setStatus($dadoObjetoServico->getStatus());
-                $servicos [] = $novoServico;
+    function buscarServicos($usuarioDao, $servicoDao, $servicoCategoriaDao, $todos = false)
+    {
+        $servicos = [];
+
+        $filtro = (Object) array();
+        $filtro->categoria = isset($_GET['categoria']) ? $_GET['categoria'] : '';
+        $filtro->descricao = isset($_GET['descricao']) ? $_GET['descricao'] : '';
+
+        $arrayDadosObjetosServico = $todos ? $servicoDao->filtrarTodosAbertos($filtro) : $servicoDao->buscarTodosAbertos();
+
+        if($arrayDadosObjetosServico != null) {
+            
+            foreach($arrayDadosObjetosServico as $dadoObjetoServico) {
+                if(auth()){
+                    if($dadoObjetoServico->getUsuarioId() != getUser()->getId()){
+                        $novoServico = new Servico();
+                        $novoServico->setId($dadoObjetoServico->getId());
+                        $novoServico->setTitulo($dadoObjetoServico->getTitulo());
+                        $novoServico->setDescricao($dadoObjetoServico->getDescricao());
+                        $novoServico->setEnderecoServico($dadoObjetoServico->getEnderecoServico());
+                        $novoServico->setValor($dadoObjetoServico->getValor());
+                        $novoServico->setUsuarioId($usuarioDao->buscarPeloId($dadoObjetoServico->getUsuarioId()));
+                        $novoServico->setDataPostagem($dadoObjetoServico->getDataPostagem());
+                        $novoServico->setStatus($dadoObjetoServico->getStatus());
+                        $servicos [] = array(
+                            'servicos' => $novoServico,
+                            'categorias' => $servicoCategoriaDao->buscarCategoriasDoServico($novoServico->getId())
+                        );
+                    }
+                } else {
+                    $novoServico = new Servico();
+                    $novoServico->setId($dadoObjetoServico->getId());
+                    $novoServico->setTitulo($dadoObjetoServico->getTitulo());
+                    $novoServico->setDescricao($dadoObjetoServico->getDescricao());
+                    $novoServico->setEnderecoServico($dadoObjetoServico->getEnderecoServico());
+                    $novoServico->setValor($dadoObjetoServico->getValor());
+                    $novoServico->setUsuarioId($usuarioDao->buscarPeloId($dadoObjetoServico->getUsuarioId()));
+                    $novoServico->setDataPostagem($dadoObjetoServico->getDataPostagem());
+                    $novoServico->setStatus($dadoObjetoServico->getStatus());
+                    $servicos [] = array(
+                        'servicos' => $novoServico,
+                        'categorias' => $servicoCategoriaDao->buscarCategoriasDoServico($novoServico->getId())
+                    );
+                }
             }
-        } else {
-            $novoServico = new Servico();
-            $novoServico->setId($dadoObjetoServico->getId());
-            $novoServico->setTitulo($dadoObjetoServico->getTitulo());
-            $novoServico->setDescricao($dadoObjetoServico->getDescricao());
-            $novoServico->setEnderecoServico($dadoObjetoServico->getEnderecoServico());
-            $novoServico->setValor($dadoObjetoServico->getValor());
-            $novoServico->setUsuarioId($usuarioDao->buscarPeloId($dadoObjetoServico->getUsuarioId()));
-            $novoServico->setDataPostagem($dadoObjetoServico->getDataPostagem());
-            $novoServico->setStatus($dadoObjetoServico->getStatus());
-            $servicos [] = $novoServico;
         }
+        return $servicos;
     }
-}
+
+    if(isset($_GET['categoria']) && isset($_GET['descricao'])){
+        $servicos = buscarServicos($usuarioDao, $servicoDao, $servicoCategoriaDao, true); //pega os serviços com filtro
+
+    } else {
+        $servicos = buscarServicos($usuarioDao, $servicoDao, $servicoCategoriaDao, false); //pega todos os serviços sem filtro
+        
+    }
+    $categoriaDao = new CategoriaDaoMysql($pdo);
+
+    $categorias = [];
+    $categoriasBanco = $categoriaDao->buscarTodas();
+
+    foreach($categoriasBanco as $c) {
+        $novaCategoria = new Categoria();
+        $novaCategoria->setId($c->getId());
+        $novaCategoria->setNome($c->getNome());
+        $categorias [] = $novaCategoria;
+    }
 
 ?>
 <div class="container-job">
@@ -55,7 +95,7 @@ if($arrayDadosObjetosServico != null) {
             <i class="fas fa-bars fa-2x ml-2"></i>
         </label>
 
-        <filtro-jobs-component></filtro-jobs-component>
+        <filtro-jobs-component categorias='<?php echo json_encode($categorias);?>' url="<?php echo $routes->jobs?>"></filtro-jobs-component>
         <list-jobs-component home="<?php echo $routes->home?>" servicos='<?php echo json_encode($servicos);?>' show_job="<?php echo $routes->jobs_show?>">
             <list-jobs-component>
     </div>
