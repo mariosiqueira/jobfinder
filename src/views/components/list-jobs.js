0afcf6c34a9b12a5cc61f71ea6c42d559a209849
jeferson <jefersongomes333@hotmail.com>
2021-01-15 19:51:48 -0300

    categorias na lista dos serviços

--- src/views/components/list-jobs.js
+++ src/views/components/list-jobs.js
@@ -22,26 +22,38 @@ var listJobsComponent = {
     },
     template: `
         <div class="d-flex flex-column p-3" id="job-list">
-            <div class="row justify-content-center m-0">
-                <div class="col-md-4 shadow rounded m-1 mb-5 pt-2 px-1 pb-0" v-for="servico in data" :key="servico.id"  style="background-color: DarkSlateGray;">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <p class="p-3">
-                            <img :src="home+'/src/files/'+servico.usuarioId.fotoPerfil" alt="img profile user" id="servico_img_user" />
-                            <span class="text-white">{{servico.usuarioId.apelido}}</span>
-                        </p>
-                        <a :href="show_job +'?s='+ servico.id" class="btn btn-success mb-4">
-                            <strong>
-                                Proposta
-                                <i class="fas fa-plus-circle    "></i>
-                            </strong>
-                        </a>
-                    </div>
-                    <div class="form-group text-center text-white">
-                        <strong class="text-uppercase"># {{servico.titulo}}</strong>
-                    </div>
-                    <p class="text-center border text-white p-5 padding-md">
-                        {{servico.descricao.substr(0, 100)+"..."}} <a :href="show_job +'?s='+ servico.id">ver mais</a>
+            <div v-if="false" class="alert alert-warning" role="alert">
+                <p class="m-0 p-1">
+                    <i class="fas fa-info-circle"></i>
+                    Nenhum serviço encontrado
+                </p>
+            </div>
+            <div v-else class="row justify-content-center m-0">
+                <div class="col-md-4 shadow rounded m-1 mb-5 pt-2 px-1 pb-0" v-for="(servico, i) in data" :key="servico.id"  style="background-color: DarkSlateGray;">
+                <div class="d-flex justify-content-between align-items-center">
+                    <p class="p-3">
+                        <img :src="home+'/src/files/'+servico.servicos.usuarioId.fotoPerfil" alt="img profile user" id="servico_img_user" />
+                        <span class="text-white">{{servico.servicos.usuarioId.apelido}}</span>
                     </p>
+                    <a :href="show_job +'?s='+ servico.servicos.id" class="btn btn-success mb-4">
+                        <strong>
+                            Proposta
+                            <i class="fas fa-plus-circle    "></i>
+                        </strong>
+                    </a>
+                </div>
+                <div class="form-group text-center text-white">
+                    <strong class="text-uppercase"># {{servico.servicos.titulo}}</strong>
+                </div>
+                <p class="text-center border text-white p-5 padding-md">
+                    {{servico.servicos.descricao.substr(0, 100)+"..."}} <a :href="show_job +'?s='+ servico.servicos.id">ver mais</a>
+                </p>
+                <hr>
+                <p class="w-100 d-flex flex-wrap ">
+                    <span class="badge badge-success m-1" v-for="c in data[i].categorias">
+                        {{c.nome}}
+                    </span>
+                </p>
                 </div>
             </div>
 
