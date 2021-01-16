b4058902ddb6f5a4082ab28a743e76c0ac5023d9
jeferson <jefersongomes333@hotmail.com>
2021-01-16 20:47:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 session_start();
+use App\Config\Conexao; 
+$pdo = Conexao::getInstance();
 
 // tratamento da url de serviço e filtro
 $jobs_show = isset($_GET['s']) ? "?s=$_GET[s]" : "";
@@ -58,12 +60,12 @@ $routes = (Object) [ //rotas nomeadas e suas respectivas url's
 ];
 
 $req = $_SERVER['REQUEST_URI']; //pega a url 
-var_dump(array(__DIR__.'/vendor/autoload.php', ));
+var_dump(array(__DIR__.'/vendor/autoload.php', $_SERVER['DOCUMENT_ROOT'], $pdo));
 
 if (array_key_exists($req, $routes_navigation)) { //verifica se a url requisitada existe nas rotas cadastrdas
-    require "./src/views/$routes_navigation[$req]"; //se existir e faz o require no arquivo da chave do array 
+    require "/src/views/$routes_navigation[$req]"; //se existir e faz o require no arquivo da chave do array 
 } else {
-    require __DIR__."/src/views/".$routes_navigation["/notFound"]; //se não existir faz o require na chave notFound
+    require "/src/views/".$routes_navigation["/notFound"]; //se não existir faz o require na chave notFound
 }
 
 function auth(){ //funçao pra verificar se o usuario está autenticado
